2a8b8a35cdc35a77b3ceb0105718fb77427d034b
devjoao98 <pheu2016@gmail.com>
2022-03-01 15:30:19 -0300

    infoSection

--- src/pages/HomePage/Data.jsx
+++ src/pages/HomePage/Data.jsx
@@ -11,7 +11,58 @@ export const homeObjOne = {
     description: '+ de 10 anos de história',
     buttonLabel: 'ENTRE EM CONTATO', 
     ligthtText: true,
-    img: '/img/undraw_investment_data_re_sh9x.svg',
+    img: '/img/undraw_growth_analytics_re_pyxf.svg',
+    alt:'Image',
+    start:''
+}
+
+export const homeObjTwo = {
+    lightBg: false,
+    primary: true,
+    ligthBg: false,
+    imgStart: '', 
+    ligthTopLine: true, 
+    ligthtTextDesc: true, 
+    topLine: 'INFORMATIZAÇÃO E CONSULTORIA PÚBLICA', 
+    headline: 'À iconp da as boas-vindas a você', 
+    description: '+ de 10 anos de história',
+    buttonLabel: 'ENTRE EM CONTATO', 
+    ligthtText: true,
+    img: '/img/img.jpg',
+    alt:'Image',
+    start:''
+}
+
+export const homeObjThree = {
+    lightBg: false,
+    primary: true,
+    ligthBg: false,
+    imgStart: '', 
+    ligthTopLine: true, 
+    ligthtTextDesc: true, 
+    topLine: 'INFORMATIZAÇÃO E CONSULTORIA PÚBLICA', 
+    headline: 'À iconp da as boas-vindas a você', 
+    description: '+ de 10 anos de história',
+    buttonLabel: 'ENTRE EM CONTATO', 
+    ligthtText: true,
+    img: '/img/undraw_in_progress_re_m1l6.svg',
+    alt:'Image',
+    start:''
+}
+
+export const homeObjFour = {
+    lightBg: false,
+    primary: true,
+    ligthBg: false,
+    imgStart: '', 
+    ligthTopLine: true, 
+    ligthtTextDesc: true, 
+    topLine: 'INFORMATIZAÇÃO E CONSULTORIA PÚBLICA', 
+    headline: 'À iconp da as boas-vindas a você', 
+    description: '+ de 10 anos de história',
+    buttonLabel: 'ENTRE EM CONTATO', 
+    ligthtText: true,
+    img: '/img/undraw_in_progress_re_m1l6.svg',
     alt:'Image',
     start:''
 }
--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
-import { homeObjOne } from './Data'
+import { 
+  homeObjOne,
+  homeObjTwo,
+  homeObjThree,
+  homeObjFour
+ } from './Data'
 import InfoSection from '../../components/InfoSection/InfoSection'
 
 export const Home = () => {
   return (
     <>
       <InfoSection {...homeObjOne}/>
+      <InfoSection {...homeObjTwo}/>
+      <InfoSection {...homeObjThree}/>
+      <InfoSection {...homeObjFour}/>
     </>
   )
 }
